229a887ca9da76551831f3e82d68a99477e91f9b
Gustaf Sterner <gsterner@gmail.com>
2013-12-21 20:37:47 +0100

    Generate start gaussian start values and plot those in single mesh.

--- phase_field_plot.py
+++ phase_field_plot.py
@@ -2,10 +2,8 @@ __author__ = 'gustaf'
 
 
 """
-Shows how to combine Normalization and Colormap instances to draw
-"levels" in pcolor, pcolormesh and imshow type plots in a similar
-way to the levels keyword argument to contour/contourf.
-
+Generates Start values for phase field simulation and
+plots result
 """
 
 import matplotlib.pyplot as plt
@@ -15,13 +13,20 @@ import numpy as np
 
 
 # make these smaller to increase the resolution
-dx, dy = 0.05, 0.05
+dx, dy = 1, 1
+
+#Grid size
+number_of_grid_points = 200
 
 # generate 2 2d grids for the x & y bounds
-y, x = np.mgrid[slice(1, 5 + dy, dy),
-                slice(1, 5 + dx, dx)]
+y, x = np.mgrid[slice(1, number_of_grid_points + dy, dy),
+                slice(1, number_of_grid_points + dx, dx)]
 
-z = np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
+#z = np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+mu, sigma = 0, 0.001 # mean and standard deviation
+z = np.random.normal(mu, sigma, x.shape)
+
 
 # x and y are bounds, so z should be the value *inside* those bounds.
 # Therefore, remove the last value from the z array.
@@ -34,23 +39,19 @@ levels = MaxNLocator(nbins=15).tick_values(z.min(), z.max())
 cmap = plt.get_cmap('PiYG')
 norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
 
-plt.subplot(2, 1, 1)
-im = plt.pcolormesh(x, y, z, cmap=cmap, norm=norm)
-plt.colorbar()
 # set the limits of the plot to the limits of the data
-plt.axis([x.min(), x.max(), y.min(), y.max()])
-plt.title('pcolormesh with levels')
 
-
-
-plt.subplot(2, 1, 2)
-# contours are *point* based plots, so convert our bound into point
+#p contours are *point* based plots, so convert our bound into point
 # centers
-plt.contourf(x[:-1, :-1] + dx / 2.,
-             y[:-1, :-1] + dy / 2., z, levels=levels,
-             cmap=cmap)
+#plt.contourf(x[:-1, :-1] + dx / 2.,
+#             y[:-1, :-1] + dy / 2., z,
+#             cmap=cmap)
+
+plt.contourf(x[:-1, :-1] + dx / 2., y[:-1, :-1] + dy / 2., z, levels=levels,cmap=cmap)
+
+
 plt.colorbar()
-plt.title('contourf with levels')
+plt.title('Field')
 
 
 plt.show()
