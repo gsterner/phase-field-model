63696455d4d3feda59fb357aba53893ab99eca13
Gustaf Sterner <gsterner@gmail.com>
2014-02-10 22:46:38 +0100

    adding use of numpy.ctypeslib for numpy communication with c-api

--- c_interface.py
+++ c_interface.py
@@ -1,21 +1,13 @@
 from ctypes import *
+import numpy as np
 
 slib   = 'libphasefieldmodela.so'
 hlib   = CDLL(slib)
 
-array_length = 10
-array_type = c_double * array_length
+n_array = np.array(range(10))
+n_array = n_array.astype(c_double)
+n_array_p = n_array.ctypes.data_as(POINTER(c_double))
 
-test_array = array_type()
-
-for i in range(10):
-    test_array[i] = i
-
-
-
-
-hlib.print_array(test_array)
-
-
+hlib.print_array(n_array_p)
 print(hlib.g_func())
 
