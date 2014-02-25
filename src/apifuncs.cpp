8dd845c4230ff3ba469b1a997aa5195ea6cb720e
Gustaf Sterner <gsterner@gmail.com>
2014-02-25 21:50:21 +0100

    Work in progress. First implementation of matrix as vector of vectors

--- src/apifuncs.cpp
+++ src/apifuncs.cpp
@@ -3,10 +3,23 @@
 #include <vector>
 using std::vector;
 
-void printInCpp()
+
+void printMatrix(vector<double> *matrix, int width, int height)
 {
-  std::cout << "tjenare"; 
+  //Needs to be rewritten for vector of vectors implementation
+  std::cout << "wifth" << width << std::endl;   
+  std::cout << "height" << height << std::endl; 
+  for(int i=0; i < width; i++)
+  {
+    for(int j=0; j < height; j++)
+    {
+      std::cout << matrix->at(i+j);
+    }  
+    std::cout << std::endl;
+  } 
+  
 }
+
 
 void copyPointerToVector(double *array_pointer, int array_size)
 {
@@ -18,6 +31,31 @@ void copyPointerToVector(double *array_pointer, int array_size)
       
   }
  
+}
+
+void copyPointerToMatrix(double *matrix_p, int rows, int cols)
+{
+
+  vector<vector<double> > data(rows, vector<double>(cols));
+  
+  for(int i=0; i < rows; i++)
+  {
+    int start = i * cols;
+    int end = (i+1) * cols;
+    vector<double>  row(matrix_p + start , matrix_p + end );
+    data.at(i) = row;   
+  }
+  ////////////////////
+  //Print matrix
+  for(int i=0; i < rows; i++)
+  {
+    for(int j=0; j < cols; j++)
+    {  
+      std::cout << data.at(i).at(j) << " ";
+    }
+    std::cout << std::endl;
+  } 
+  ///////////////////
 }
 
 extern "C"
@@ -36,6 +74,11 @@ extern "C"
 	printf("%f\n", test_array[i]);
       }
     return;
+  }
+
+  void print_matrix(double *matrix_p, int rows, int cols)
+  {
+    copyPointerToMatrix(matrix_p, rows, cols);
   }
 
 
