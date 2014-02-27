ce49e44e82015206a5be87ce43b5daba59629e15
Gustaf Sterner <gsterner@gmail.com>
2014-02-27 20:17:44 +0100

    splitting up matrix creation from api

---
+++ src/grid.h
@@ -0,0 +1,12 @@
+#include <vector>
+using std::vector;
+
+void printMatrix(vector<double> *matrix, int width, int height);
+
+void copyPointerToVector(double *array_pointer, int array_size);
+
+void copyPointerToMatrix(double *matrix_p, int rows, int cols);
+
+
+
+
