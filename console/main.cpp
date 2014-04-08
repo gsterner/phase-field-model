d6d297a33263fa531ddc468c1a1042a026ca37c9
Gustaf Sterner <gsterner@gmail.com>
2014-04-08 20:27:58 +0200

    testing c++ api with protobuf

--- console/main.cpp
+++ console/main.cpp
@@ -7,14 +7,16 @@ using namespace std;
 int main(int argc, char* argv[]) {
   cout << "console file "<< argv[1] << endl;
 
+  phasefield::SetupData sim_data;
+
   // Read the existing address book.
   fstream input(argv[1], ios::in | ios::binary);
   if (!input) {
     cout << argv[1] << ": File not found." << endl;
   }
+  sim_data.ParseFromIstream(&input);
 
-  phasefield::SetupData sim_data;
-  
+  cout << sim_data.time_steps() << endl;
 
   return 0;
 }
