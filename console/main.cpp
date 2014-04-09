8ac049eb144cdef5bc88676dfdb642b6317572ab
Gustaf Sterner <gsterner@gmail.com>
2014-04-09 22:25:22 +0200

    protobuf code for serializing phase field as matrix. Concept test

--- console/main.cpp
+++ console/main.cpp
@@ -16,7 +16,23 @@ int main(int argc, char* argv[]) {
   }
   sim_data.ParseFromIstream(&input);
 
-  cout << sim_data.time_steps() << endl;
+  cout << "time steps:" << sim_data.time_steps() << endl;
+  cout << "rows " << sim_data.phase_field().row_size() << endl;
+  cout << "cols " << sim_data.phase_field().row(0).element_size() << endl;
+  cout << "first element: " << sim_data.phase_field().row(0).element(0) << endl;
+
+  int rows = sim_data.phase_field().row_size();
+  int cols = sim_data.phase_field().row(0).element_size();
+
+  for(int i = 0; i < rows; i++) 
+  {
+    for(int j = 0; j < cols; j++)
+    {
+      cout << sim_data.phase_field().row(i).element(j) << " ";
+    }
+    cout << endl;
+  }
+
 
   return 0;
 }
--- console/setup_simulation.py
+++ console/setup_simulation.py
@@ -4,6 +4,25 @@ import simulation_data_pb2
 sim_data = simulation_data_pb2.SetupData()
 
 sim_data.time_steps = 20
+
+field = sim_data.Field()
+#field = sim_data.phase_field.add()
+
+in_data = [[1,2,3,11],[4,5,6,44],[7,8,9,77]]
+
+for in_row in in_data:
+    new_row = field.row.add()
+    for r in in_row:
+        new_row.element.append(r)
+
+sim_data.phase_field.MergeFrom(field)
+
+print(len(field.row))
+print(len(field.row[0].element))
+print(sim_data.phase_field)
+
+
+
 f = open("simulation_setup.data", "wb")
 f.write(sim_data.SerializeToString())
 f.close()
